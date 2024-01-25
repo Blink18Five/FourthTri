7c7d70eb60e757e4f6a7302d0b7f39fb5aac9dab
Jacob Best <bestja210@gmail.com>
2024-01-25 10:16:06 -0500

    Initial Commit

---
+++ FourthTri/FourthTriApp.swift
@@ -0,0 +1,17 @@
+//
+//  FourthTriApp.swift
+//  FourthTri
+//
+//  Created by Jacob Best on 1/25/24.
+//
+
+import SwiftUI
+
+@main
+struct FourthTriApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
