44c65987a0f9632e9fe57affcd1c024649d066de
Jacob Best <bestja210@gmail.com>
2024-01-31 11:39:05 -0500

    Added Stuff

---
+++ FourthTri/Models/Classes.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  FourthTri
+//
+//  Created by Jacob Best on 1/30/24.
+//
+
+import Foundation
+
+protocol Class {
+    var date: Date { get }
+    var name: String { get }
+    var description: String { get }
+}
---
+++ FourthTri/Views/Achievements.swift
@@ -0,0 +1,20 @@
+//
+//  Achievements.swift
+//  FourthTri
+//
+//  Created by Jacob Best on 1/31/24.
+//
+
+import SwiftUI
+
+struct Achievements: View {
+    var body: some View {
+        NavigationLink("Help") {
+            ContentView()
+        }
+    }
+}
+
+#Preview {
+    Achievements()
+}
