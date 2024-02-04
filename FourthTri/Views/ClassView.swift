bcf78ac477d4f15f38f6012f6430f589cf0ed386
Jacob Best <bestja210@gmail.com>
2024-02-04 04:34:38 -0500

    Updates to the fourthtri app

---
+++ FourthTri/Views/ClassView.swift
@@ -0,0 +1,127 @@
+//
+//  ClassView.swift
+//  FourthTri
+//
+//  Created by Jacob Best on 2/4/24.
+//
+
+import SwiftUI
+
+struct ClassesView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct FeaturedClassesView: View {
+    var body: some View {
+        FeaturedScrollView(element: "Classes", elementName: "Class Name", elementDescription:  "description description description\ndescription description description\ndescription description ...") {
+            ClassesView()
+        }
+    }
+}
+
+struct FeaturedScrollView<Destination> : View where Destination : View{
+    var element: String
+    var elementName: String
+    var elementDescription: String
+    var elementDestination: () -> Destination
+    
+    var body: some View {
+        VStack(spacing: 0) {
+            HStack(alignment: .center, spacing: 0) {
+                Text(element)
+                    .font(.system(size: 24))
+                    .bold()
+                    .padding(.bottom, 10)
+                    .padding([.leading, .top])
+                Spacer()
+                NavigationLink(destination: elementDestination()) {
+                    HStack {
+                        Text("All \(element)")
+                        Image(systemName: "chevron.right")
+                    }
+                    .font(.system(size: 11))
+                    .padding(.bottom, 10)
+                    .padding([.trailing, .top])
+                }
+            }
+            ScrollView(.horizontal) {
+                LazyHGrid(rows: [GridItem()]) {
+                    ElementView(elementName: elementName, elementDescription: elementDescription)
+                    ElementView(elementName: elementName, elementDescription: elementDescription)
+                    ElementView(elementName: elementName, elementDescription: elementDescription)
+                }
+            }
+            .padding(.leading)
+        }
+    }
+}
+
+struct ElementView: View {
+    var elementName: String
+    var elementDescription: String
+    
+    var body: some View {
+        ZStack {
+            HStack {
+                VStack {
+                    RoundedRectangle(cornerRadius: 10)
+                        .foregroundStyle(Color.powderpink)
+                        .frame(width: 89, height: 89)
+                        .padding(.leading, 30)
+                        .padding(.trailing, 50)
+                        .padding(.bottom, 120)
+                    Spacer()
+                }
+                VStack {
+                    Spacer()
+                    RoundedRectangle(cornerRadius: 10)
+                        .foregroundStyle(Color.powderpink)
+                        .frame(width: 89, height: 89)
+                    Spacer()
+                }
+                Spacer()
+            }
+            VStack(spacing: 0) {
+                Image("family")
+                    .resizable()
+                    .frame(width: 163, height: 99)
+                    .clipShape(RoundedRectangle(cornerRadius: 8.0))
+                HStack {
+                    Text(elementName)
+                        .font(.system(size: 16))
+                        .bold()
+                        .padding([.top, .bottom], 8)
+                        .padding(.leading, 55)
+                    Spacer()
+                }
+                HStack(alignment: .top, spacing: 0) {
+                    Text(elementDescription)
+                        .font(.system(size: 8))
+                        .padding(.trailing, 5)
+                    VStack {
+                        Circle()
+                            .frame(width: 23, height: 23)
+                            .foregroundStyle(Color.tertiary)
+                            .overlay {
+                                Image(systemName: "info")
+                                    .foregroundStyle(.white)
+                            }
+                            .padding(.top, 25)
+                            .padding(.bottom, 6)
+                    }
+                }
+            }
+        }
+        .frame(width: 180, height: 180)
+        .shadow(radius: 2)
+        .clipShape(
+            RoundedRectangle(cornerRadius: 10)
+        )
+    }
+}
+
+#Preview {
+    FeaturedClassesView()
+}
---
+++ FourthTri/Views/ForYouView.swift
@@ -0,0 +1,155 @@
+//
+//  ForYouView.swift
+//  FourthTri
+//
+//  Created by Jacob Best on 2/4/24.
+//
+
+import SwiftUI
+
+struct ForYouView: View {
+    var body: some View {
+        NavigationStack {
+            HStack {
+                NavigationLink(destination: Profile()) {
+                    Image("profile")
+                        .resizable()
+                        .frame(width: 61, height: 61)
+                        .clipShape(Circle())
+                }
+                .padding(.leading)
+                Spacer()
+                NavigationLink(destination: Achievements()) {
+                    Image(systemName: "bell")
+                        .font(.system(size: 28))
+                }
+                NavigationLink(destination: Achievements()) {
+                    Circle()
+                        .frame(width: 45)
+                        .overlay {
+                            Image(systemName: "qrcode")
+                                .font(.system(size: 28))
+                                .foregroundStyle(.white)
+                        }
+                }
+                .padding(.trailing)
+            }
+            .foregroundStyle(Color("tertiary"))
+            .padding(.bottom)
+            ScrollView {
+                VStack {
+                    HStack {
+                        RoundedRectangle(cornerRadius: 15.0)
+                            .stroke(Color("forYouGray1"), lineWidth: 1)
+                            .frame(width: 174,height: 116)
+                            .foregroundColor(.white)
+                            .overlay{
+                                VStack(spacing: .zero) {
+                                    taskHeaders(title: "Facility Hours", sfImage: "clock")
+                                    Text("12")
+                                        .bold()
+                                        .font(.system(size: 52))
+                                }
+                            }
+                        
+                        
+                        RoundedRectangle(cornerRadius: 15.0)
+                            .stroke(Color("forYouGray1"), lineWidth: 1)
+                            .frame(width: 174,height: 116)
+                            .foregroundColor(.white)
+                            .overlay{
+                                VStack {
+                                    taskHeaders(title: "Daily Tasks", sfImage: "timer")
+                                        .padding(.top, 5.0)
+                                    VStack {
+                                        dailyTask()
+                                        dailyTask()
+                                    }
+                                }
+                            }
+                    }
+                    
+                    MyScheduleView()
+                    
+                    FeaturedClassesView()
+                    
+                    FeaturedEventsView()
+                    
+                }
+                ZStack(alignment: .leading) {
+                    Image("finalForYou")
+                        .resizable()
+                        .frame(width: 358, height: 116)
+                        .overlay {
+                            RadialGradient(
+                                gradient: Gradient(colors: [.clear, .black]),
+                                center: .topTrailing,
+                                startRadius: .zero,
+                                endRadius: CGFloat(600.0)
+                            )
+                        }
+                        .clipShape(RoundedRectangle(cornerRadius: 8.0))
+                        .padding()
+                    Text("The most essential\ngift to give")
+                        .multilineTextAlignment(.leading)
+                        .font(.system(size: 25))
+                        .foregroundStyle(Color.white)
+                        .bold()
+                        .padding(.leading, 25)
+                }
+            }
+        }
+    }
+}
+
+func dailyTask(_ taskName: String = "Daily Task") -> some View {
+    return HStack {
+        RoundedRectangle(cornerRadius: 5.0)
+            .frame(width: 23, height: 21)
+            .foregroundColor(Color("powderpink"))
+            .padding(.leading)
+            .overlay{
+                RoundedRectangle(cornerRadius: 3.0)
+                    .frame(width: 19, height: 17)
+                    .padding(.leading)
+                    .foregroundColor(.white)
+            }
+        ZStack(alignment: .leading){
+            ZStack(alignment: .leading) {
+                RoundedRectangle(cornerRadius: 5.0)
+                    .frame(height: 25)
+                    .padding(.trailing)
+                    .foregroundColor(.ourGray)
+                Text("\(taskName)")
+                    .font(.system(size: 11))
+                    .bold()
+                    .padding(.leading, 20)
+            }
+            UnevenRoundedRectangle(cornerRadii:RectangleCornerRadii(topLeading: 5.0, bottomLeading: 5.0))
+                .frame(width: 10, height: 25)
+                .foregroundColor(Color("tertiary"))
+        }
+    }
+}
+
+func taskHeaders(title: String, sfImage: String) -> some View {
+    return HStack(alignment: .center) {
+        RoundedRectangle(cornerRadius: 8.0)
+            .frame(width: 35, height: 35, alignment: .topLeading)
+            .foregroundColor(Color("powderpink"))
+            .overlay{
+                Image(systemName: sfImage)
+                    .bold()
+                    .foregroundColor(.white)
+            }
+            .padding(.leading)
+        Text(title)
+            .font(.system(size: 16.0))
+            .bold()
+        Spacer()
+    }
+}
+
+#Preview {
+    ForYouView()
+}
