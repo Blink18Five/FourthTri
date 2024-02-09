6f215967722658a31d396037b4a0368670f6b3a2
Jordan Allen <jordanallen93@gmail.com>
2024-02-09 09:43:02 -0600

    Achcievement Grid

--- FourthTri/Views/ProgressView.swift
+++ FourthTri/Views/ProgressView.swift
@@ -7,12 +7,42 @@
 
 import SwiftUI
 
+
 struct ProgressView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
-
-#Preview {
-    ProgressView()
-}
+        VStack {
+            let columns = [
+                GridItem(.flexible()),
+                GridItem(.flexible()),
+                GridItem(.flexible()),
+                
+            ]
+            
+            ScrollView{
+                LazyVGrid(columns: columns, spacing: 20) {
+                    ForEach(0..<15) {index in
+                        RoundedRectangle(cornerRadius: 10)
+                            .fill(Color.gray)
+                            .frame(height: 100)
+                            .overlay(
+                                Text("Item\(index)")
+                                    .foregroundColor(.white)
+                            )
+                        
+                    }
+                }
+                .padding()
+                    }
+                    .navigationTitle("Grid View")
+                
+                }
+            }
+        }
+        
+        
+        struct ProgressView_Previews: PreviewProvider {
+            static var previews: some View {
+                ProgressView()
+            }
+        }
+    
