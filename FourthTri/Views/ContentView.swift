c4a68e70573bf38322f013a3aa92c6497b753383
Jacob Best <bestja210@gmail.com>
2024-02-14 09:48:01 -0500

    Random Stuff

--- FourthTri/Views/ContentView.swift
+++ FourthTri/Views/ContentView.swift
@@ -8,28 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var selection = 1
+//    @State private var selection = 2
     
     var body: some View {
-        TabView(selection: $selection) {
+        TabView {
             ForYouView()
                 .tabItem {
                     Image(systemName: "doc.text.image")
                     Text("For You")
                 }
-                .tag(1)
             ProgressView()
                 .tabItem {
                     Image(systemName: "trophy")
                     Text("Progress")
                 }
-                .tag(2)
             PackagesView()
                 .tabItem {
                     Image(systemName: "shippingbox")
                     Text("Packages")
                 }
-                .tag(3)
             ProfileView()
                 .tabItem {
                     Image(systemName: "person.circle")
