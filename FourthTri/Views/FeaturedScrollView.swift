7b7b93047b341c635d628a78860e0d345464cd71
Jacob Best <bestja210@gmail.com>
2024-02-17 17:39:38 -0500

    Stylistic changes to both the code and the Edit Settings Page

---
+++ FourthTri/Views/FeaturedScrollView.swift
@@ -0,0 +1,18 @@
+//
+//  FeaturedScrollView.swift
+//  FourthTri
+//
+//  Created by Jacob Best on 2/16/24.
+//
+
+import SwiftUI
+
+struct FeaturedScrollView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FeaturedScrollView()
+}
---
+++ FourthTri/Views/GridElementView.swift
@@ -0,0 +1,18 @@
+//
+//  GridElementView.swift
+//  FourthTri
+//
+//  Created by Jacob Best on 2/16/24.
+//
+
+import SwiftUI
+
+struct GridElementView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GridElementView()
+}
